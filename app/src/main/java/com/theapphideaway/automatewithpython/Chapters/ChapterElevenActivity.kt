13b9da92547259158b6a059e8ec42632e8306551
ianschoenrock <theapphideaway@gmail.com>
2018-12-31 07:00:58 -0500

    Created layout for chapter eleven

---
+++ app/src/main/java/com/theapphideaway/automatewithpython/Chapters/ChapterElevenActivity.kt
@@ -0,0 +1,12 @@
+package com.theapphideaway.automatewithpython.Chapters
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChapterElevenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chapter_eleven)
+    }
+}
