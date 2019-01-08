8de48f53d0e3e80ece26de279e8b9497b9691d40
ianschoenrock <theapphideaway@gmail.com>
2019-01-08 10:09:17 -0500

    Added updated layout for chapter 16

--- app/src/main/java/com/theapphideaway/automatewithpython/Chapters/ChapterSixteenActivity.kt
+++ app/src/main/java/com/theapphideaway/automatewithpython/Chapters/ChapterSixteenActivity.kt
@@ -2,6 +2,8 @@ package com.theapphideaway.automatewithpython.Chapters
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.chaquo.python.Python
 import com.theapphideaway.automatewithpython.R
@@ -15,18 +17,34 @@ class ChapterSixteenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chapter_sixteen)
+    }
 
-        send_button.setOnClickListener {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
-            var username = username_edit_text.text.toString()
-            var password = password_edit_text.text.toString()
-            var recipient = recipient_edit_text.text.toString()
-            var subject = subject_edit_text.text.toString()
-            var body = body_edit_text.text.toString()
+        menuInflater.inflate(R.menu.menu_chapter_sixteen, menu)
 
-            var result = pyClass.callAttr("send_email", username, password,recipient,subject,body).toString()
+        return super.onCreateOptionsMenu(menu)
+    }
 
-            Toast.makeText(this, result, Toast.LENGTH_LONG).show()
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if(item != null){
+            when(item.itemId){
+                R.id.send_button_item ->{
+
+                    var username = username_edit_text.text.toString()
+                    var password = password_edit_text.text.toString()
+                    var recipient = recipient_edit_text.text.toString()
+                    var subject = subject_edit_text.text.toString()
+                    var body = body_edit_text.text.toString()
+
+                    var result = pyClass.callAttr("send_email", username, password,recipient,subject,body).toString()
+
+                    Toast.makeText(this, result, Toast.LENGTH_LONG).show()
+                }
+            }
         }
+
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/python/ChapterThree.py
+++ app/src/main/python/ChapterThree.py
@@ -2,5 +2,6 @@ def exception_handling(givenNumber):
     try:
         return givenNumber / 0
     except ZeroDivisionError:
-        return "Sorry we can't divide by zero for you"
+        return "Sorry we can't divide by zero for you. Solve this problem by changing the source code. " \
+               "This is meant to be an example of exception handling"
 
